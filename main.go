9622a2b8ae14cd3810320a2f6dd18705d2fb7e0c
Francisco Coromoto <coromoto.francisco@gmail.com>
2021-11-19 06:42:25 -0300

    mudando hello world

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello world"))
+		writer.Write([]byte("Hello world !!!"))
 	})
 
 	http.ListenAndServe(":8080", nil)
